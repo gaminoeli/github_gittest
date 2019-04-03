ce00be3caa8838d3ea2ce94f671e0caa66c5aa36
gaminoeli <gaminoeli@sina.com>
2019-04-03 13:43:13 +0800

    modify maohoatest.cpp 4times

--- maohaotest.cpp
+++ maohaotest.cpp
@@ -1,4 +1,7 @@
 
 maohaotest.cpp first modify
-second modify testing: git push orgin : 在非master工作分支
-third modify testing: git push orgin : 在master分支
+second modify testing: git push orgin : 在非master分支，推送名称相同的两地分支
+third modify testing: git push orgin : 在master分支，推送名称相同的两地分支
+fourth modify testing: git push orgin : 在master分支，推送名称不同的两地分支
+
+conclusion：推送名称相同的两地分支，在不在master，有没有：，都会正确推送。
